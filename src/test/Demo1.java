44559891e4ed209ddb7baee31862c54fa4f23806
rovshenklichev <kulieyev@gmail.com>
2021-02-18 16:40:57 -0800

    Initial commit

---
+++ src/test/Demo1.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+    @Test
+    public void Testcase1(){
+        System.out.println(" ok this is 1 ");
+    }
+    @Test
+    public void Testcase2(){
+        System.out.println("Ok this is second test case 2");
+    }
+    @Test
+    public void Testcase3(){
+        System.out.println("Ok this is second test case 3");
+    }
+}
---
+++ src/test/Demo2.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+        @Test
+        public void Testcase4(){
+            System.out.println("This is 1st Test case 4");
+        }
+        @Test
+        public void Testcase5(){
+            System.out.println("Ok this is second test case 5");
+        }
+        @Test
+        public void Testcase6(){
+            System.out.println("Ok this is second test case 6");
+
+    }
+}
